8dae962ca044adea14b18be15ee51eade93bc743
kiranpe <kiranwls2011@gmail.com>
2021-09-14 21:15:33 +0530

    AWS VPC Configuration

--- modules/vpc_variables.tf
+++ modules/vpc_variables.tf
@@ -33,13 +33,13 @@ variable "vpc_cidr" {
 variable "public_subnets_cidr" {
   description = "Public Subnet CIDR Block"
   type        = list(any)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.5.0/24"]
 }
 
 variable "private_subnets_cidr" {
   description = "Private Subnet CIDR Block"
   type        = list(any)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.0.3.0/24", "10.0.4.0/24", "10.0.6.0/24"]
 }
 
 variable "availability_zones" {
